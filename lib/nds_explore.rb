891d57a00a0ccf2db39d84f7a5ed05404d278486
Radouane Bahi <radbahi@gmail.com>
2019-12-03 01:31:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,16 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+<<<<<<< HEAD
   titleCounter = 0 
   while titleCounter < directors_database[0][:movies].length do
     puts directors_database[0][:movies][titleCounter][:title]
+=======
+  pp(directors_database)
+  titleCounter = 0 
+  while titleCounter < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][:title]
+>>>>>>> a85b68c6c7bccf4b04555ffada575f0075e70a47
     titleCounter += 1 
   end
 end
